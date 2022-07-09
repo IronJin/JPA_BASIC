fcbdbe0103011292f0a6ee5d01036de30fcbb7be
IronJin <85132068+IronJin@users.noreply.github.com>
2022-07-09 22:18:16 +0900

    second commit

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -19,18 +19,69 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+//            //Member findMember = em.find(Member.class, 1L);
+//            //JPQL 을 예시
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(8)
+//                    .getResultList();
+//
+//            //JPQL 을 사용하여 받아온 List 를 출력하기
+//            for (Member member : result) {
+//                System.out.println("member.getName() = " + member.getName());
+//            }
+//
+//            /**
+//             * 영속성 컨텍스트 예시
+//             */
+//            //비영속
+//            Member member = new Member();
+//            member.setId(100L);
+//            member.setName("HelloPersistenceContext");
+//
+//            //영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member); //1차 캐시에 저장
+//            System.out.println("=== AFTER ===");
+//
+//            //1차캐시에서 꺼내서 findMember를 가져오기 때문에 DB가 아닌 1차캐시에서 꺼내오는 거임
+//            Member findMember = em.find(Member.class, 100L);
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
+
+//            //----------------------------------------------------
+//            //jpa는 영속 entity의 동일성을 보장한다
+//            Member findMember = em.find(Member.class, 1L);
+//            Member findMember2 = em.find(Member.class, 1L);
+//            System.out.println("result = "+ (findMember==findMember2));
+
+
+//            //-------------------
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            //영속 컨텍스트에 쌓이고 있음
+//            em.persist(member1);
+//            em.persist(member2);
+
+
+//            //==================================
+//            Member member = em.find(Member.class, 150L);
+//            member.setName("zzzz");
+//
+//            System.out.println("=========================");
+
+//            //-----------------------------------
+//            Member member = new Member(200L, "member200");
+//            em.persist(member);
+//            //tx.commit 이 되기전에 sql이 반영이 됨
+//            em.flush();
+//            System.out.println("===============================");
+//            //--------------------------------------
 
 
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
-            
+            //트랜잭션을 적용
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hello/jpa/Member.java
+++ src/main/java/hello/jpa/Member.java
@@ -1,31 +1,35 @@
 package hello.jpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 //@Table(name = "USER") //Target 테이블 설정하는 방법임
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column(name = "name", nullable = false)
+    private String username;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+    public Member() {
+    }
+
 }
